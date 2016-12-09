0a87bff14c5d8bf4e5fc6b5b71f4e360403be57c
Luan Au <luanau@gmail.com>
2016-12-09 21:14:38 +0000

    Initial commit

---
+++ client/components/Main.js
@@ -0,0 +1,16 @@
+import {Link} from 'react-router';
+
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1><Link to="/">Reduxstagram</Link></h1>
+                {React.cloneElement(this.props.children, this.props)}
+            </div>
+        );
+    }
+}
+
+export default Main;
---
+++ client/store.js
@@ -0,0 +1,22 @@
+import {createStore, compse} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+
+//Import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//Create default this.this.state.
+const defaultState = {
+    posts,
+    comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
